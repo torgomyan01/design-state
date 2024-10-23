cccdae37673f4fb51c30f3cd553b4ea227d6ba65
Andranik Torgomyan <and.torgomyan01@gmail.com>
2024-10-23 23:35:27 +0400

    Add new pagegit push

--- js/script.js
+++ js/script.js
@@ -21,35 +21,26 @@ const {
 // }
 
 
-$('.home-slider').slick({
-  dots: true,
-  infinite: false,
-  speed: 300,
-  slidesToShow: 4,
-  slidesToScroll: 4,
-  centerPadding: '60px',
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-      }
+window.addEventListener('DOMContentLoaded', function (){
+  const swiper = new Swiper('.home-slider', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
     },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
+    // navigation: {
+    //   nextEl: '.swiper-button-next',
+    //   prevEl: '.swiper-button-prev',
+    // },
+    breakpoints: {
+      992: {
+        slidesPerView: 4,
+        spaceBetween: 30,
+      },
+      0: {
+        slidesPerView: 1, // 1 սլայդ մոբայլում
+        spaceBetween: 30, // Կամ ցանկացած ցանկալի տարածություն
+      },
     },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        centerMode: true,
-      }
-    }
-  ]
-});
+  });
+})
